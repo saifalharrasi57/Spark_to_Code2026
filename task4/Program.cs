1ca5befdb8f8e32187f54f7acff68677b82bc5ea
saif alharrasi <saifalharrasi57@gmail.com>
2026-07-07 15:49:30 +0400

    the first 3 tasks-> done

--- task4/Program.cs
+++ task4/Program.cs
@@ -2,8 +2,48 @@
 
 class Program
 {
+    //task1
+    // static void PrintWelcome(string username)
+    // {
+    //     Console.Write($"welcome to spark to code mr{username}");
+    // }
+    
+    
+    // task2
+    // static double sqr(int value)
+    // {
+    //     double result = Math.Pow(value, 2);
+    //     Console.Write($" the result is!!!!  {result}");
+    //     return result;
+    // }
+    
+    
+    // // task3
+    // static double CelsiusToFahrenheit(double C)
+    // {
+    //     double result = (C * 9 / 5) + 32;
+    //     return result;
+    // }
+    
+    // task4
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Console.Write("dear user , enter your name ");
+        // string user_name = Console.ReadLine();
+        // PrintWelcome(user_name);
+        
+        
+       //  Console.Write("dear user cn you insert a number to see it's square");
+       // int number = int.Parse(Console.ReadLine());
+       //  double result = sqr(number);
+       //  
+       //
+       // Console.Write("dear user , could you please insert the temprature in Celsius ( btw must be in double e.g 2.7 : ");
+       //
+       //     double temp = double.Parse(Console.ReadLine());
+       //     Console.WriteLine($"in fahrenhiet {CelsiusToFahrenheit(temp)}");
+       //
+       //
+
     }
 }

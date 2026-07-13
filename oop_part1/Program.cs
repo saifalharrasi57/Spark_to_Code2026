c3822d69f72aafd95bbf50807afe0df558b73183
saif alharrasi <saifalharrasi57@gmail.com>
2026-07-13 10:38:36 +0400

    bankaccounk class-> added

---
+++ oop_part1/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace oop_part1;
+
+
+
+public class BankAccount
+{
+    public int accountnumber;
+    public string holdername;
+    public double balance;
+
+
+    public void Deposit(double amount)
+    {
+        balance += amount;
+        SendEmail();
+        
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (balance < amount)
+        {
+            Console.WriteLine("the withdraw amount is larger than the  balance");
+        }
+        else
+        {
+            balance -= amount;
+            SendEmail();
+        }
+        
+    }
+
+    public double CheckBalance()
+    {
+        PrintInformation();
+        return balance;
+    }
+
+    private void PrintInformation()
+    {
+        Console.WriteLine($"account holdername:{holdername}");
+        Console.WriteLine($"account balance: {balance}");
+        
+    }
+
+    private void SendEmail()
+    {
+        Console.WriteLine($"[Notification] An email update has been sent to the account holder for Account #{accountnumber}.");
+    }
+    
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
